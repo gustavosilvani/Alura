fab9b7bd02fad4f71b87526d31e03d44e3393a24
gustavosilvani <gustavosilvani@hotmail.com>
2022-03-22 11:38:32 -0300

    Update 1-MeuPrimeiroPrograma.js

--- 1-MeuPrimeiroPrograma.js
+++ 1-MeuPrimeiroPrograma.js
@@ -1,1 +1,11 @@
-console.log("Meu Primeiro programa");
+console.log("Meu Primeiro programa: trabalhando com variáveis");
+
+const idade = 21;
+
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
