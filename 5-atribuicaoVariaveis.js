45d0b0e6a8eb3b858fa64b6836791a89bd0e50dc
gustavosilvani <gustavosilvani@hotmail.com>
2022-03-23 23:34:46 -0300

    atribuicao De Variaveis

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com Atribuição de Variáveis");
+
+const idade = 21;
+let primeiroNome = "Gustavo";
+const sobrenome = "Silvani";
+
+//console.log(nome + "" + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto = 2;
